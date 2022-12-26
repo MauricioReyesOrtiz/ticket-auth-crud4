8f84701a32737b5e25cd3a78b756b44bac62ec27
Mauricio <mauricioreyesortiz1997@gmail.com>
2022-12-26 16:53:13 -0400

    thrirtyone commit

--- api/serializers.py
+++ api/serializers.py
@@ -63,7 +63,7 @@ class TicketServicioSerializer(serializers.ModelSerializer):
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'first_name', 'last_name', 'username', 'password', 'groups', 'email', 'superuser_status'] #'is_superuser', 'active', 'staff_status', Permissions 
+        fields = ['id', 'first_name', 'last_name', 'username', 'password', 'groups', 'email', 'Permissions'] #'is_superuser', 'active', 'staff_status', Permissions ,superuser_status
          #esconder password
         extra_kwargs = {
             'password': {'write_only': True, 'required': True}
