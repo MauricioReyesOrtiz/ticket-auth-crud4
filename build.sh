02dd6a0ae4fcd2f10056ac6be2284bb0e332ae60
Mauricio <mauricioreyesortiz1997@gmail.com>
2022-12-26 07:23:13 -0400

    twentytwo commit

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 set -o errexit
 
 # poetry install
-pip install -r requirements.txt
+pip install --upgrade pip -r requirements.txt 
 
 python manage.py collectstatic --no-input
 python manage.py migrate
