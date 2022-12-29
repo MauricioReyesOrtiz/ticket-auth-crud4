f3582830f35d6407b1c35b22a3b9627768d78552
Mauricio <mauricioreyesortiz1997@gmail.com>
2022-12-29 19:25:55 -0400

    fiftytwo commit

--- api/views.py
+++ api/views.py
@@ -69,8 +69,6 @@ class UserViewSet(viewsets.ModelViewSet):  #ReadOnlyModelViewSet
     serializer_class = UserSerializer
     permission_classes = [permissions.AllowAny,]
     #authentication_classes = [authentication.TokenAuthentication,]
-    
-    
     #permission_classes = [permissions.IsAdminUser,]
     
     
@@ -85,17 +83,17 @@ class LoginView(views.APIView): #viewsets.ModelViewSet    views.APIView
     #authentication_classes = [authentication.BasicAuthentication]
     def post(self, request):
         # Recuperamos las credenciales y autenticamos al usuario
-        username = request.data.get('username', None)
-        password = request.data.get('password', None)
-        if username is None or password is None:
+        username2= request.data.get('username', None)
+        password2 = request.data.get('password', None)
+        if username2 is None or password2 is None:
             return response.Response({'message': 'Please provide both username and password'},status=status.HTTP_400_BAD_REQUEST)
-        user = authenticate(username=username, password=password)
-        if not user:
+        user2 = authenticate(username=username2, password=password2)
+        if not user2:
             return response.Response({'message': 'Usuario o Contraseña incorrecto !!!! '},status=status.HTTP_404_NOT_FOUND)
 
-        token, _ = Token.objects.get_or_create(user=user)
+        token, _ = Token.objects.get_or_create(user=user2)
         # Si es correcto añadimos a la request la información de sesión
-        if user:
+        if user2:
             # para loguearse una sola vez
             # login(request, user)
             return response.Response({'message':'usuario y contraseña correctos!!!!'},status=status.HTTP_200_OK)
