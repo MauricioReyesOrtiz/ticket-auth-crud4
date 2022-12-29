fd1be4d9dc6bc7d90f972d3c76bbb45a1a91be38
Mauricio <mauricioreyesortiz1997@gmail.com>
2022-12-29 03:52:26 -0400

    fiftyone commit

--- api/views.py
+++ api/views.py
@@ -85,17 +85,17 @@ class LoginView(views.APIView): #viewsets.ModelViewSet    views.APIView
     #authentication_classes = [authentication.BasicAuthentication]
     def post(self, request):
         # Recuperamos las credenciales y autenticamos al usuario
-        username2= request.data.get('username', None)
-        password2 = request.data.get('password', None)
-        if username2 is None or password2 is None:
+        username = request.data.get('username', None)
+        password = request.data.get('password', None)
+        if username is None or password is None:
             return response.Response({'message': 'Please provide both username and password'},status=status.HTTP_400_BAD_REQUEST)
-        user2 = authenticate(username=username2, password=password2)
-        if not user2:
+        user = authenticate(username=username, password=password)
+        if not user:
             return response.Response({'message': 'Usuario o Contraseña incorrecto !!!! '},status=status.HTTP_404_NOT_FOUND)
 
-        token, _ = Token.objects.get_or_create(user=user2)
+        token, _ = Token.objects.get_or_create(user=user)
         # Si es correcto añadimos a la request la información de sesión
-        if user2:
+        if user:
             # para loguearse una sola vez
             # login(request, user)
             return response.Response({'message':'usuario y contraseña correctos!!!!'},status=status.HTTP_200_OK)
